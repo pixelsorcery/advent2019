53574458d23a9d8f53d84c2bd0bb31a4021bdd63
pixelsorcery <4041512+pixelsorcery@users.noreply.github.com>
2020-01-24 23:59:01 -0500

    day 18 part 2

--- 18/app.js
+++ 18/app.js
@@ -17,28 +17,40 @@ function isDoor(char)
 }
 
 let allKeysMask = 0;
-
+let keys = [];
 let width = 0;
 let height = 0;
 let startPos = [];
-for(let i = 0; i < input.length; i++) {
-    if (input[i] === '\n') {
-        if (width === 0) width = i+1;
-        height++;
-    }
+let keyLocations = {};
+function processInput(map) {
+    for(let i = 0; i < map.length; i++) {
+        if (map[i] === '\n') {
+            if (width === 0) width = i+1;
+            height++;
+        }
 
-    if (input[i] === '@') {
         let ypos = height;
         let xpos = i % width;
-        startPos.push([xpos,ypos]);
-    }
 
-    if (isKey(input[i])) {
-        allKeysMask |= 1 << (input[i].charCodeAt(0) - 'a'.charCodeAt(0));
+        if (map[i] === '@') {
+            startPos.push([xpos,ypos]);
+            keyLocations[startPos.length.toString()] = [xpos, ypos];
+        }
+
+        if (isKey(map[i])) {
+            allKeysMask |= 1 << (map[i].charCodeAt(0) - 'a'.charCodeAt(0));
+            keyLocations[map[i]] = [xpos, ypos];
+            keys.push(map[i]);
+        }
     }
 }
 
-let part1 = 9999999;
+function setBit(mask, bit){
+    let newMask = mask | 1 << (bit.charCodeAt(0) - 'a'.charCodeAt(0));
+    return newMask;
+}
+
+processInput(input);
 
 function haveKey(mask, char) {
     let hasKey = false;
@@ -49,77 +61,240 @@ function haveKey(mask, char) {
     return hasKey;
 }
 
-function makeMemoKey(x, y, mask) {
-    return x + ',' + y + ',' + mask;
+function makeMemoKey(pos, mask) {
+    let key = "";
+
+    for (let i in pos) {
+        key += pos[i][0].toString() + ',' + pos[i][1].toString() + ',';
+    }
+    return key + mask.toString();
 }
 
-function haveBeenHereBefore(x, y, mask, dist) {
-    let key = makeMemoKey(x, y, mask);
+function haveBeenHereBefore(pos, mask, dist) {
+    let key = makeMemoKey(pos, mask);
     let result = false;
     if (typeof pathMemo[key] !== "undefined") {
         if (pathMemo[key] <= dist) {
             result = true;
         }
-        else {
-            pathMemo[key] = dist;
-        }
-    } else {
-        pathMemo[key] = dist;
     }
+    pathMemo[key] = dist;
+    return result;
+}
 
+function haveBeenHereBeforeIdx(idx, mask, dist) {
+    let key = idx.toString() + "," + mask.toString();
+    let result = false;
+    if (typeof pathMemo[key] !== "undefined") {
+        if (pathMemo[key] <= dist) {
+            result = true;
+        }
+    }
+    pathMemo[key] = dist;
     return result;
 }
 
 const dirs = [{x:0, y:1}, {x:0, y:-1}, {x:1, y:0}, {x:-1, y:0}];
 
 let pathMemo = {};
-let curQueue = [];
 
-for (let i = 0; i < startPos.length; i++) {
-    curQueue.push({x:startPos[i][0], y:startPos[i][1], dist:0, mask:0});
+function run(map) {
+    let curQueue = [];
+    let answer = 999999;
+    let keyQueue = [];
+    let maxDist = 0;
+    curQueue.push({pos:startPos.slice(), dist:0, mask:0});
+    while (curQueue.length > 0) {
+        // pop the front (bfs)
+        let state = curQueue.shift();
+
+        if( haveBeenHereBefore(state.pos, state.mask, state.dist)) {
+            continue;
+        }
+
+        for (let h = 0; h < state.pos.length; h++) {
+            for (let i = 0; i < dirs.length; i++) {
+                // get next position
+                let posIdx = (state.pos[h][1]+dirs[i].y) * width + (state.pos[h][0] + dirs[i].x);
+
+                let newPos = state.pos.slice();
+                newPos[h]  = state.pos[h].slice(); // makes a copy
+                newPos[h][0] += dirs[i].x;
+                newPos[h][1] += dirs[i].y;
+
+                let newState = {pos:newPos, dist:state.dist+1, mask:state.mask};
+
+                if (map[posIdx] != '#') {
+                    if (isDoor(map[posIdx])) {
+                        if (haveKey(state.mask, map[posIdx])) {
+                            curQueue.push(newState);
+                        }
+                    } else if (isKey(map[posIdx]) && 
+                                !(newState.mask & (1 << (map[posIdx].charCodeAt(0) - 'a'.charCodeAt(0))))) {
+                        newState.mask |= 1 << (map[posIdx].charCodeAt(0) - 'a'.charCodeAt(0));
+
+                        if (newState.mask === allKeysMask) {
+                            if (newState.dist < answer) {
+                                answer = newState.dist;
+                            }
+                            continue;
+                        }
+
+                        curQueue.push(newState);
+                    } else {
+                        curQueue.push(newState);
+                    }
+                } 
+            }
+        }
+    }
+    return answer;
 }
 
-while (curQueue.length > 0) {
-    // pop the front (bfs)
-    let state = curQueue.shift();
+//console.log("part 1:", run(input));
 
-    if( haveBeenHereBefore(state.x, state.y, state.mask, state.dist)) {
-        continue;
+// part 2: different approach, bfs maze search is too slow for 4 robots at once,
+//         try dfs dijkstra style algo using only key positions
+
+function findShortestPath(map, a, b) {
+    let startPos = keyLocations[a];
+    let endPos = keyLocations[b];
+    let doors = 0;
+    let queue = [];
+    queue.push([startPos, doors, 0]);
+    let visited = {};
+
+    while (queue.length > 0) {
+        let state = queue.shift();
+
+        if (typeof visited[state[0]] === "undefined") {
+            visited[state[0]] = 1;
+        } else {
+            continue;
+        }
+
+        for (let i = 0; i < dirs.length; i++) {
+            // get next position
+            let posIdx = (state[0][1]+dirs[i].y) * width + (state[0][0] + dirs[i].x);
+            let newPos = state[0].slice();
+            let doors = 
+            newPos[0] += dirs[i].x;
+            newPos[1] += dirs[i].y;
+
+            let newMask = state[1];
+
+            if (map[posIdx] == '#') {
+                continue;
+            }
+
+            if (isDoor(map[posIdx])) {
+                newMask = setBit(state[1], map[posIdx].toLowerCase());
+                queue.push([newPos, newMask, state[2] + 1]);
+                continue;
+            } else if (newPos[0] === endPos[0] && newPos[1] === endPos[1]) {
+                return [state[2] + 1, state[1]];
+            } else {
+                queue.push([newPos, newMask, state[2] + 1]);
+            }
+        }
     }
+    // if no path was found we end up here
+    return [-1, 0];
+}
 
-    for (let i = 0; i < dirs.length; i++) {
-        // get next position
-        let posIdx = (state.y+dirs[i].y) * width + (state.x + dirs[i].x);
+function processPaths() {
+    for (let i = 0; i < keys.length; i++) {
+        for (let j = 0; j < keys.length; j++) {
+            if (i === j) continue;
+            let path = findShortestPath(input2, keys[i], keys[j]);
+            if (path[0] > -1)
+            keyPaths[[keys[i],keys[j]]] = path;
+        }
+    }
+}
 
-        let newState = {x:state.x+dirs[i].x, y:state.y+dirs[i].y, dist:state.dist+1, mask:state.mask};
+function run2(map) {
+    let startState = {robots:['1','2','3','4'], mask:0, dist:0};
+    
+    let memo = {};
 
-        if (input[posIdx] != '#') {
-            if (isDoor(input[posIdx])) {
-                if (haveKey(state.mask, input[posIdx])) {
-                    curQueue.push(newState);
-                } else {
-                    curQueue.push(state);
-                }
-            } else if (isKey(input[posIdx]) && 
-                        !(newState.mask & (1 << (input[posIdx].charCodeAt(0) - 'a'.charCodeAt(0))))) {
-                // add new location in if it's nearer than a previous location and key is the same
-                newState.mask |= 1 << (input[posIdx].charCodeAt(0) - 'a'.charCodeAt(0));
+    let answer = 0;
 
-                if (newState.mask === allKeysMask) {
-                    if (newState.dist < part1) {
-                        part1 = newState.dist;
-                        pathMemo[newState.x + ','+ newState.y + ',' + newState.mask] = newState.dist;
-                        console.log("potential answer:", part1);
-                    }
+    let queue = [];
+    queue.push(startState);
+
+    while (queue.length > 0) {
+        // run algo
+        let state = queue.pop();
+
+        if (answer > 0 && state.dist >= answer) {
+            continue;
+        }
+
+        for (let i = 0; i < state.robots.length; i++) {
+            // find all paths for robot
+            for (let k = 0; k < keys.length; k++) {
+                let key = [state.robots[i], keys[k]];
+                if (typeof keyPaths[key] === "undefined") {
                     continue;
                 }
+                let keyPath = keyPaths[key];
 
-                curQueue.push(newState);
-            } else {
-                curQueue.push(newState);
+                if ((state.mask & keyPath[1]) === keyPath[1]) {
+                    let newRobots = state.robots.slice();
+                    newRobots[i] = keys[k];
+                    let newMask = 0;
+                    newMask = state.mask | 1 << (keys[k].charCodeAt(0) - 'a'.charCodeAt(0));
+
+                    if (newMask === state.mask) {
+                        continue;
+                    }
+                    if (newMask === allKeysMask) {
+                        if (answer === 0) {
+                            answer = state.dist + keyPath[0];
+                        } else if (state.dist + keyPath[0] < answer) {
+                            answer = state.dist + keyPath[0];
+                            console.log("potential answer:", answer);
+                        }
+                        continue;
+                    }
+
+                    let curKey = [newRobots, newMask];
+                    if (typeof memo[curKey] !== "undefined" && 
+                        memo[curKey] <= state.dist + keyPath[0] ) {
+                        continue;
+                    }
+
+                    memo[curKey] = state.dist + keyPath[0];
+
+                    let newState = {robots:newRobots, mask:newMask, dist:(state.dist + keyPath[0])}
+                    queue.push(newState);
+                }
             }
-        } 
+        }
     }
+
+    return answer;
 }
 
-console.log("part 1:", part1);
+const input2 = fs.readFileSync("input2.txt").toString();
+
+// reset everything
+startPos    = [];
+pathMemo    = {};
+keys        = [];
+width       = 0;
+height      = 0;
+allKeysMask = 0;
+keyLocations = {};
+let keyPaths = {};
+processInput(input2);
+
+keys.push("1");
+keys.push("2");
+keys.push("3");
+keys.push("4");
+
+processPaths();
+
+console.log("part 2:", run2(input2));
